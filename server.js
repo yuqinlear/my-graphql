729b9edcc8b2a02a349972bb660d3a20aaa62d6c
yuqinlear <yuqinlear@gmail.com>
2018-04-05 08:50:09 -0400

    feat: test the api with arguments and object type

--- server.js
+++ server.js
@@ -1,26 +1,51 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var {buildSchema} = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
-    hello: String
+    getDie(numSides: Int): RandomDie
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// The root provides the top-level API endpoints
 var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
-};
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  }
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
